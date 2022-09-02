a7c45f6ef31ad00620f2711a65eb278290ece05a
FriedrichSpruck <friedrichspruck@yahoo.de>
2022-09-02 11:18:12 +0200

    added solution for 'Printf exercise 2'

--- MyWorkspace/host/000TestProject/main.c
+++ MyWorkspace/host/000TestProject/main.c
@@ -9,8 +9,7 @@
 
 int main(void){
 
-	printf("David says,  \"Programming is fun!\" \n");
-	printf("Good day!\n");
+	printf("David says , \"Programming is fun!\" \n**Conditions apply , \"Offers valid until tomorrow\" \nC:\\My computer\My folder\nD:/My documents/My file \n\ \\ \\ \\ Today is holiday \\ \\ \\ \ \nThis is a triple quoted string \"\"\" This month has 30 days \"\"\" \n");
 
 	printf("Press 'Enter' to exit this application\n");
 
